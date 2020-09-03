1e6ad60eda47f79b4a3b5812e8085a493136b7b2
Abdulaziz-al <abdoal0aziz133@gmail.com>
2020-09-03 21:02:37 +0300

    save

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,13 +29,9 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('البريد الإلكتروني') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email">
+                                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                              
                             </div>
                         </div>
                         <div class="form-group row">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title> تحدي ام القرى لليوم الوطني </title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
